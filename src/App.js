3139ec7ab6a39c3887e43ba1fec66b49a2b98a7a
unfeeling-001 <hyanerkaev@gmail.com>
2023-10-06 11:30:13 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Base from './Base';
+
+const App = () => {
+  return (
+    <div>
+      <Base />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-      <p>so cute</p>
-);
+root.render(<App />);
+
